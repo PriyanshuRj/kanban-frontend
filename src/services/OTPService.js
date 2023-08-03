09924c38258b0558d73092b62259a4ddbfac958d
PriyanshuRj <priyanshurajput0071109@gmail.com>
2023-08-04 01:18:29 +0530

    Updated OTP service

--- src/services/OTPService.js
+++ src/services/OTPService.js
@@ -3,7 +3,9 @@ import URL from './URL';
 
 export async function SubmitEmailOTPService(data){
     try{
+        console.log({data});
         const res = await axios.post(URL + "otpverify",data);
+        console.log({res})
         return res;
     }
     catch(error){
